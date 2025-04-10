5972653c67c224a8101c2be251dcd211284b949f
Rashid Enahora <rashidenahora@MacBookPro.attlocal.net>
2025-04-09 23:12:45 -0400

    Completed Select Component

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,67 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
       {children}
-    </select>
+      </NativeSelect>
+      <PresentationalBit>
+        {displayedValue} 
+        <IconWrapper style={{'--size': 24 + 'px'}}>
+          <Icon id="chevron-down" strokeWidth={1} size={24} />
+        </IconWrapper>
+      </PresentationalBit>
+    </Wrapper>
   );
 };
+
+/* Is width: max-content applying width based upon the
+width of the childrens content???*/ 
+const Wrapper = styled.div`
+  //isolation: isolate;     /* Strategy1: z-idex */
+  position: relative;
+  width: max-content;
+`;
+
+const NativeSelect = styled.select`
+  //z-index: 2;    /* Strategy1: z-idex */
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  appearance: none;
+`;
+
+const PresentationalBit = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  color: ${COLORS.gray700};
+  font-size: ${16/16}rem;
+  padding: 12px 16px;
+  padding-right: 52px;
+  border-radius: 8px;
+  /* State based styles */
+  ${NativeSelect}:focus + & {
+    outline: 1px dotted #212121;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+
+  ${NativeSelect}:hover + & {
+    color: ${COLORS.black};
+  }
+`;
+
+const IconWrapper = styled.div`
+  //z-index: 1;   /* Strategy1: z-index */
+  pointer-events: none; /* Strategy2: Eliminate ALL mouse events */
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 10px;
+  margin: auto;
+  width: var(--size);
+  height: var(--size);
+`;
 
 export default Select;
