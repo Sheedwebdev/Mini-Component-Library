57233e12d15b15fab68273c23b760fdbb330f911
Rashid Enahora <rashidenahora@MacBookPro.attlocal.net>
2025-04-11 00:35:35 -0400

    Completed half of the IconInput component

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -11,9 +11,44 @@ const IconInput = ({
   icon,
   width = 250,
   size,
-  placeholder,
+  ...delegated /* Need better understanding!!!*/
 }) => {
-  return <strong>{width}</strong>;
+  return (
+    <Wrapper>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <IconWrapper style={{'--size': 16 + 'px'}}>
+        <Icon id={icon} size={16}/>
+      </IconWrapper>
+      <TextInput {...delegated}/>    
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.label`
+  display: block;
+  position: relative;
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto 0;
+  height: var(--size);
+`;
+
+const TextInput = styled.input`
+  height: ${24 / 16}rem;
+  border: none;
+  border-bottom: 1px solid ${COLORS.black};
+  padding-left: 24px;
+  color: ${COLORS.gray700};
+  font-weight: 700;
+
+  &::placeholder {
+    color: ${COLORS.gray500};
+    font-weight: 400;
+  }
+`;
 
 export default IconInput;
